a65db323fea46f21ba17eac2181db8b69f58f5ca
Michael Gant <your_email_here>
2021-12-17 16:05:16 -0500

    FRONT=END STYING-SINGLE-PAGE-VIEW

--- components/Layout.js
+++ components/Layout.js
@@ -3,8 +3,32 @@ import Head from "next/head";
 import NextLink from "next/link";
 import useStyles from "../utils/style";
 import { AppBar, Container, Toolbar, Typography, Link } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 
 export default function Layout({ description, title, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#ecb41a",
+      },
+      secondary: {
+        main: "#f0c040",
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -12,26 +36,29 @@ export default function Layout({ description, title, children }) {
         <title>{title ? `${title} - Next Ebazon` : "Next Ebazon"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>Ebazon</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>Ebazon</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>All rights reserve. Ebazon</footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>All rights reserve. Ebazon</footer>
+      </ThemeProvider>
     </div>
   );
 }
