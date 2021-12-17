471ae09e56b26f9481bfec9f264a2704fd08dccf
Michael Gant <your_email_here>
2021-12-17 13:21:57 -0500

    FRONT-END ADDING-PRODUCTS

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,22 @@
 import React, { Children } from 'react'
 import Head from 'next/head'
+import useStyles from '../utils/style';
 import { AppBar, Container, Toolbar, Typography } from '@mui/material'
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Ebazon</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Ebazon</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
-        All rights reserve. Ebazon
-      </footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>All rights reserve. Ebazon</footer>
     </div>
   );
 }
