1c35417de446a710b32934645bf6b85a06b25e23
Michael Gant <your_email_here>
2021-12-20 12:05:15 -0500

    changes

--- pages/cart.js
+++ pages/cart.js
@@ -31,8 +31,6 @@ function CartScreen() {
     cart: { cartItems },
   } = state;
   const updateCartHandler = async (item, quantity) => {
-    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${item._id}`);
     if (data.countInStock < quantity) {
       window.alert("Sorry. Product is out of stock");
