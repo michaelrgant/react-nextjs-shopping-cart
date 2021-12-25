219f76859453d38be2afa47c071a0067763db760
Michael Gant <your_email_here>
2021-12-25 14:21:48 -0500

    updates

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,16 +12,15 @@ import {
   TextField,
   CircularProgress,
 } from '@material-ui/core';
-// import Rating from '@material-ui/lab/Rating';
 import Layout from '../../components/Layout';
 import useStyles from '../../utils/style';
 import Product from '../../models/Products';
 import db from '../../utils/db';
 import axios from 'axios';
 import { Store } from '../../utils/Store';
-// import { getError } from '../../utils/error';
+ import { getError } from '../../utils/error';
 import { useRouter } from 'next/router';
-// import { useSnackbar } from 'notistack';
+ import { useSnackbar } from 'notistack';
 
 export default function ProductScreen(props) {
   const router = useRouter();
@@ -29,7 +28,7 @@ export default function ProductScreen(props) {
   const { userInfo } = state;
   const { product } = props;
   const classes = useStyles();
-  // const { enqueueSnackbar } = useSnackbar();
+   const { enqueueSnackbar } = useSnackbar();
 
   const [reviews, setReviews] = useState([]);
   const [rating, setRating] = useState(0);
@@ -59,21 +58,21 @@ export default function ProductScreen(props) {
     }
   };
 
-  const fetchReviews = async () => {
-    try {
-      const { data } = await axios.get(`/api/products/${product._id}/reviews`);
-      setReviews(data);
-    } catch (err) {
-      // enqueueSnackbar(getError(err), { variant: 'error' });
-    }
-  };
-  useEffect(() => {
-    fetchReviews();
-  }, []);
+  // const fetchReviews = async () => {
+  //   try {
+  //     const { data } = await axios.get(`/api/products/${product._id}/reviews`);
+  //     setReviews(data);
+  //   } catch (err) {
+  //     // enqueueSnackbar(getError(err), { variant: 'error' });
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchReviews();
+  // }, []);
 
-  if (!product) {
-    return <div>Product Not Found</div>;
-  }
+  // if (!product) {
+  //   return <div>Product Not Found</div>;
+  // }
 
    const addToCartHandler = async () => {
 
@@ -171,77 +170,6 @@ export default function ProductScreen(props) {
         </Grid>
       </Grid>
       <List>
-        <ListItem>
-          <Typography name="reviews" id="reviews" variant="h2">
-            Customer Reviews
-          </Typography>
-        </ListItem>
-        {reviews.length === 0 && <ListItem>No review</ListItem>}
-        {reviews.map((review) => (
-          <ListItem key={review._id}>
-            <Grid container>
-              <Grid item className={classes.reviewItem}>
-                <Typography>
-                  <strong>{review.name}</strong>
-                </Typography>
-                <Typography>{review.createdAt.substring(0, 10)}</Typography>
-              </Grid>
-              <Grid item>
-                <Rating value={review.rating} readOnly></Rating>
-                <Typography>{review.comment}</Typography>
-              </Grid>
-            </Grid>
-          </ListItem>
-        ))}
-        <ListItem>
-          {userInfo ? (
-            <form onSubmit={submitHandler} className={classes.reviewForm}>
-              <List>
-                <ListItem>
-                  <Typography variant="h2">Leave your review</Typography>
-                </ListItem>
-                <ListItem>
-                  <TextField
-                    multiline
-                    variant="outlined"
-                    fullWidth
-                    name="review"
-                    label="Enter comment"
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                  />
-                </ListItem>
-                <ListItem>
-                  {/* <Rating
-                    name="simple-controlled"
-                    value={rating}
-                    onChange={(e) => setRating(e.target.value)}
-                  /> */}
-                </ListItem>
-                <ListItem>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                  >
-                    Submit
-                  </Button>
-
-                  {loading && <CircularProgress></CircularProgress>}
-                </ListItem>
-              </List>
-            </form>
-          ) : (
-            <Typography variant="h2">
-              Please{' '}
-              <Link href={`/login?redirect=/product/${product.slug}`}>
-                login
-              </Link>{' '}
-              to write a review
-            </Typography>
-          )}
-        </ListItem>
       </List>
     </Layout>
   );
